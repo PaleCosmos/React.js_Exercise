18c0f7564e26940b233507c072ca2672a955e8f0
Sanghyeon Park <tkdgusdlqhek@hanmail.net>
2019-10-27 02:34:24 +0900

    Learn about LifeCycle of React.js

--- src/App.js
+++ src/App.js
@@ -2,30 +2,69 @@ import React, { Component } from 'react';
 import './App.css';
 import Movie from './Movie';
 
-const movieTitles = [
-  "Matrix",
-  "Full Metal Jacker",
-  "OldBoy",
-  "Star Wars"
-];
-
-const movieImages = [
-  "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_.jpg",
-  "https://m.media-amazon.com/images/M/MV5BNzkxODk0NjEtYjc4Mi00ZDI0LTgyYjEtYzc1NDkxY2YzYTgyXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg",
-  "https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Oldboykoreanposter.jpg/220px-Oldboykoreanposter.jpg",
-  "https://m.media-amazon.com/images/M/MV5BOTAzODEzNDAzMl5BMl5BanBnXkFtZTgwMDU1MTgzNzE@._V1_.jpg"
-];
+const movies = [
+  {
+    title: "Matrix",
+    poster: "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_.jpg",
+  },
+  {
+    title: "Full Metal Jacket",
+    poster: "https://m.media-amazon.com/images/M/MV5BNzkxODk0NjEtYjc4Mi00ZDI0LTgyYjEtYzc1NDkxY2YzYTgyXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg"
+  },
+  {
+    title: "OldBoy",
+    poster: "https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Oldboykoreanposter.jpg/220px-Oldboykoreanposter.jpg",
+  },
+  {
+    title: "Star Wars",
+    poster: "https://m.media-amazon.com/images/M/MV5BOTAzODEzNDAzMl5BMl5BanBnXkFtZTgwMDU1MTgzNzE@._V1_.jpg"
+  },
+  {
+    title: "아기 공룡 둘리",
+    poster: "https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Ft1.daumcdn.net%2Fcfile%2Ftistory%2F27217937558634C21A"
+  }
+]
 
 class App extends Component {
+
+  UNSAFE_componentWillMount(){
+this.log('componentWillMount')
+  }
+
+  UNSAFE_componentWillReceiveProps(){
+this.log('componentWillReceiveProps')
+  }
+
+  shouldComponentUpdate(){
+this.log('shouldComponentUpdate')
+  }
+
+  UNSAFE_componentWillUpdate(){
+this.log('componentWillUpdage')
+  }
+
   render() {
+    this.log('render')
     return (
       <div className="app">
-        <Movie title={movieTitles[0]} poster = {movieImages[0]}/>
-        <Movie title={movieTitles[1]} poster = {movieImages[1]}/>
-        <Movie title={movieTitles[2]} poster = {movieImages[2]}/>
-        <Movie title={movieTitles[3]} poster = {movieImages[3]}/>
+        {movies.map((movie, index) => {
+          return <Movie key={index} title={movie.title} poster={movie.poster} />
+        })}
       </div>
     );
+  }
+
+  componentDidUpdate(){
+this.log('componentDidUpdate')
+  }
+
+  componentDidMount()
+  {
+    this.log('componentDidMout')
+  }
+
+  log(str){
+    console.log(str)
   }
 }
 
--- src/Movie.js
+++ src/Movie.js
@@ -1,25 +1,37 @@
 import React, { Component } from 'react';
 import './Movie.css';
+import propTypes from 'prop-types'
 
 class Movie extends Component {
+
+  static propTypes = {
+    title: propTypes.string.isRequired,
+    poster: propTypes.string.isRequired
+  }
+
   render() {
     console.log(this.props)
     return (
       <div >
-          <MoviePoster URL = {this.props.poster}/>
-          <h1>
-            {this.props.title}
-          </h1>
-          <br/>
+        <MoviePoster URL={this.props.poster} />
+        <h1>
+          {this.props.title}
+        </h1>
+        <br />
       </div>
     );
   }
 }
 
-class MoviePoster extends Component{
-  render(){
+class MoviePoster extends Component {
+
+  static propTypes = {
+    URL : propTypes.string.isRequired
+  }
+
+  render() {
     return (
-      <img src = {this.props.URL} width = {300}/>
+      <img alt='alt' src={this.props.URL} width='300' />
     )
   }
 }
